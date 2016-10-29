22f600759b5d0d45b5ff718d4f8851108661a6e0
lzy <lizhiyue243@163.com>
2016-10-29 15:32:12 +0800

    dd

---
+++ app/src/main/java/com/example/tony/tonydemo/widget/ItemView.java
@@ -0,0 +1,27 @@
+package com.example.tony.tonydemo.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by lzy on 2016/10/29.
+ */
+
+public class ItemView extends LinearLayout {
+    public ItemView(Context context) {
+        super(context);
+    }
+
+    public ItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ItemView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public ItemView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
